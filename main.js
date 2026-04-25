a238531f13f8485e45f0ed770aea07d56f303920
eduwon221 <eduwon221@gmail.com>
2026-04-25 06:00:39 +0000

    Update project files for product builder lecture

--- main.js
+++ main.js
@@ -1,1 +1,61 @@
-// Add JS here
+class LottoBall extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+    }
+
+    connectedCallback() {
+        const number = this.getAttribute('number');
+        const color = this.getAttribute('color');
+
+        this.shadowRoot.innerHTML = `
+            <style>
+                .ball {
+                    width: 100px;
+                    height: 100px;
+                    border-radius: 50%;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    font-size: 2em;
+                    font-weight: bold;
+                    color: white;
+                    background-color: ${color};
+                    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2), inset 0 -4px 6px rgba(0, 0, 0, 0.3);
+                }
+            </style>
+            <div class="ball">${number}</div>
+        `;
+    }
+}
+
+customElements.define('lotto-ball', LottoBall);
+
+
+document.getElementById('generator-btn').addEventListener('click', () => {
+    const lottoBallsContainer = document.getElementById('lotto-balls');
+    lottoBallsContainer.innerHTML = ''; 
+    const numbers = new Set();
+
+    while(numbers.size < 6) {
+        numbers.add(Math.floor(Math.random() * 45) + 1);
+    }
+
+    const sortedNumbers = Array.from(numbers).sort((a, b) => a - b);
+
+    sortedNumbers.forEach(number => {
+        const lottoBall = document.createElement('lotto-ball');
+        lottoBall.setAttribute('number', number);
+        lottoBall.setAttribute('color', getRandomColor());
+        lottoBallsContainer.appendChild(lottoBall);
+    });
+});
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
